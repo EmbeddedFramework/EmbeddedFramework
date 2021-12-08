2d07be37ce15398a8365946c1ba139f809d62e4d
Gustavo Muro <gustmuro@gmail.com>
2021-12-08 10:08:41 -0300

    work in i2c

--- modules/bsp_frdmkl46z/src/bsp_frdmkl46z_i2c.c
+++ modules/bsp_frdmkl46z/src/bsp_frdmkl46z_i2c.c
@@ -35,10 +35,12 @@
 
 /*==================[inclusions]=============================================*/
 #include "bsp_frdmkl46z_i2c.h"
+#include "fsl_i2c.h"
 
 /*==================[macros and typedef]=====================================*/
 
 /*==================[internal functions declaration]=========================*/
+static i2c_master_handle_t m_handle;
 
 /*==================[internal data definition]===============================*/
 
@@ -47,15 +49,86 @@ efHal_dh_t efHal_dh_I2C0;
 
 /*==================[internal functions definition]==========================*/
 
-static efHal_i2c_ec_t bsp_frdmkl46z_i2c_deviceTransfer(void* param, void *pTx, size_t sTx, void *pRx, size_t sRx)
+static efHal_i2c_ec_t bsp_frdmkl46z_i2c_deviceTransfer(void* param, efHal_i2c_devAdd_t da, void *pTx, size_t sTx, void *pRx, size_t sRx)
 {
-    return EF_HAL_I2C_EC_NO_ERROR;
+    efHal_i2c_ec_t ret = EF_HAL_I2C_EC_NO_ERROR;
+    i2c_master_transfer_t masterXfer;
+
+    memset(&masterXfer, 0, sizeof(masterXfer));
+    masterXfer.slaveAddress = da;
+    masterXfer.flags = kI2C_TransferDefaultFlag;
+
+    if (pRx == NULL || sRx == 0)
+        masterXfer.direction = kI2C_Read;
+    else
+    {
+        masterXfer.direction = kI2C_Write;
+        if (pTx == NULL || sTx == 0)
+            ret = EF_HAL_I2C_EC_INVALID_PARAMS;
+    }
+
+    if (ret == EF_HAL_I2C_EC_NO_ERROR)
+    {
+        /* transmission equal or less than 4 byte */
+        if (sTx <= sizeof(masterXfer.subaddress))
+        {
+            int i;
+
+            masterXfer.subaddress = 0;
+
+            for (i = 0 ; i < sTx ; i++)
+                masterXfer.subaddress |= ((uint8_t*)pTx)[i] << (8*i);
+
+            masterXfer.subaddressSize = sTx;
+            masterXfer.data = pRx;
+            masterXfer.dataSize = sRx;
+        }
+        else
+        {   /* transmission greater than 4 bytes */
+
+            /* if there aren't reception, use only data for transfer */
+            if (pRx == NULL || sRx == 0)
+            {
+                masterXfer.subaddress = 0;
+                masterXfer.subaddressSize = 0;
+                masterXfer.data = pTx;
+                masterXfer.dataSize = sTx;
+            }
+            /* TODO: implements transfer in two part */
+            else
+            {
+                ret = EF_HAL_I2C_EC_TRANSFER_UNSUPPORTED;
+            }
+        }
+    }
+
+    if (ret == EF_HAL_I2C_EC_NO_ERROR)
+    {
+        I2C_MasterTransferNonBlocking(param, &m_handle, &masterXfer);
+    }
+
+    return ret;
+}
+
+static void i2c_master_callback(I2C_Type *base, i2c_master_handle_t *handle, status_t status, void *userData)
+{
+    if (status == kStatus_Success)
+    {
+    //    completionFlag = true;
+    }
+
+    if ((status == kStatus_I2C_Nak) || (status == kStatus_I2C_Addr_Nak))
+    {
+     //   nakFlag = true;
+    }
 }
 
 /*==================[external functions definition]==========================*/
 extern void bsp_frdmkl46z_i2c_init(void)
 {
-    efHal_dh_I2C0 = efHal_i2c_deviceReg(bsp_frdmkl46z_i2c_deviceTransfer, NULL);
+    efHal_dh_I2C0 = efHal_i2c_deviceReg(bsp_frdmkl46z_i2c_deviceTransfer, I2C0);
+
+    I2C_MasterTransferCreateHandle(I2C0, &m_handle, i2c_master_callback, efHal_dh_I2C0);
 }
 
 
