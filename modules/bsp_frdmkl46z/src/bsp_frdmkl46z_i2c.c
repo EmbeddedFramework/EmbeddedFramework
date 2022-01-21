eb7426f3546710eeb1a1c2f17a9c09e9c4cc0670
Gustavo Muro <gustmuro@gmail.com>
2022-01-21 07:51:08 -0300

    I2C is now working fine

--- modules/bsp_frdmkl46z/src/bsp_frdmkl46z_i2c.c
+++ modules/bsp_frdmkl46z/src/bsp_frdmkl46z_i2c.c
@@ -36,9 +36,21 @@
 /*==================[inclusions]=============================================*/
 #include "bsp_frdmkl46z_i2c.h"
 #include "fsl_i2c.h"
+#include "fsl_port.h"
+#include "fsl_gpio.h"
 #include "efHal_internal.h"
 
 /*==================[macros and typedef]=====================================*/
+
+#define I2C_BAUDRATE            100000U
+
+#define I2C_RELEASE_SDA_PORT PORTE
+#define I2C_RELEASE_SCL_PORT PORTE
+#define I2C_RELEASE_SDA_GPIO GPIOE
+#define I2C_RELEASE_SDA_PIN 25U
+#define I2C_RELEASE_SCL_GPIO GPIOE
+#define I2C_RELEASE_SCL_PIN 24U
+#define I2C_RELEASE_BUS_COUNT 100U
 
 /*==================[internal functions declaration]=========================*/
 static i2c_master_handle_t m_handle;
@@ -59,47 +71,37 @@ static efHal_i2c_ec_t bsp_frdmkl46z_i2c_deviceTransfer(void* param, efHal_i2c_de
     masterXfer.slaveAddress = da;
     masterXfer.flags = kI2C_TransferDefaultFlag;
 
-    if (pRx == NULL || sRx == 0)
-        masterXfer.direction = kI2C_Read;
-    else
+    /* there aren't reception */
+    if (sRx == 0 || pRx == NULL)
     {
-        masterXfer.direction = kI2C_Write;
         if (pTx == NULL || sTx == 0)
             ret = EF_HAL_I2C_EC_INVALID_PARAMS;
+        else
+        {
+            masterXfer.direction = kI2C_Write;
+            masterXfer.subaddress = 0;
+            masterXfer.subaddressSize = 0;
+            masterXfer.data = pTx;
+            masterXfer.dataSize = sTx;
+        }
     }
-
-    if (ret == EF_HAL_I2C_EC_NO_ERROR)
+    else
     {
-        /* transmission equal or less than 4 byte */
-        if (sTx <= sizeof(masterXfer.subaddress))
+        masterXfer.direction = kI2C_Read;
+
+        if (sTx > sizeof(masterXfer.subaddress))
+            ret = EF_HAL_I2C_EC_TRANSFER_UNSUPPORTED;
+        else if (pTx != NULL && sTx > 0)
         {
             int i;
-
             masterXfer.subaddress = 0;
 
             for (i = 0 ; i < sTx ; i++)
-                masterXfer.subaddress |= ((uint8_t*)pTx)[i] << (8*i);
+                masterXfer.subaddress |= ((uint8_t*)pTx)[sTx-i-1] << (8*i);
 
             masterXfer.subaddressSize = sTx;
             masterXfer.data = pRx;
             masterXfer.dataSize = sRx;
-        }
-        else
-        {   /* transmission greater than 4 bytes */
-
-            /* if there aren't reception, use only data for transfer */
-            if (pRx == NULL || sRx == 0)
-            {
-                masterXfer.subaddress = 0;
-                masterXfer.subaddressSize = 0;
-                masterXfer.data = pTx;
-                masterXfer.dataSize = sTx;
-            }
-            /* TODO: implements transfer in two part */
-            else
-            {
-                ret = EF_HAL_I2C_EC_TRANSFER_UNSUPPORTED;
-            }
         }
     }
 
@@ -123,7 +125,7 @@ static void i2c_master_callback(I2C_Type *base, i2c_master_handle_t *handle, sta
 
         case kStatus_I2C_Nak:
         case kStatus_I2C_Addr_Nak:
-            ec = EF_HAL_I2C_EC_NO_ERROR;
+            ec = EF_HAL_I2C_EC_NAK;
             break;
 
         default:
@@ -134,9 +136,123 @@ static void i2c_master_callback(I2C_Type *base, i2c_master_handle_t *handle, sta
     efHal_internal_i2c_endOfTransfer(userData, ec);
 }
 
+static void i2c_configPins(void)
+{
+    const port_pin_config_t porte24_pin31_config = {/* Internal pull-up resistor is enabled */
+                                                    kPORT_PullUp,
+                                                    /* Slow slew rate is configured */
+                                                    kPORT_SlowSlewRate,
+                                                    /* Passive filter is disabled */
+                                                    kPORT_PassiveFilterDisable,
+                                                    /* Low drive strength is configured */
+                                                    kPORT_LowDriveStrength,
+                                                    /* Pin is configured as I2C0_SCL */
+                                                    kPORT_MuxAlt5};
+    CLOCK_EnableClock(kCLOCK_PortE);
+
+    /* PORTE24 (pin 31) is configured as I2C0_SCL */
+    PORT_SetPinConfig(PORTE, 24U, &porte24_pin31_config);
+
+    const port_pin_config_t porte25_pin32_config = {/* Internal pull-up resistor is enabled */
+                                                    kPORT_PullUp,
+                                                    /* Slow slew rate is configured */
+                                                    kPORT_SlowSlewRate,
+                                                    /* Passive filter is disabled */
+                                                    kPORT_PassiveFilterDisable,
+                                                    /* Low drive strength is configured */
+                                                    kPORT_LowDriveStrength,
+                                                    /* Pin is configured as I2C0_SDA */
+                                                    kPORT_MuxAlt5};
+    /* PORTE25 (pin 32) is configured as I2C0_SDA */
+    PORT_SetPinConfig(PORTE, 25U, &porte25_pin32_config);
+}
+
+static void i2c_config(void)
+{
+    i2c_master_config_t masterConfig;
+    uint32_t sourceClock;
+    /*
+     * masterConfig.baudRate_Bps = 100000U;
+     * masterConfig.enableStopHold = false;
+     * masterConfig.glitchFilterWidth = 0U;
+     * masterConfig.enableMaster = true;
+     */
+    I2C_MasterGetDefaultConfig(&masterConfig);
+
+    masterConfig.baudRate_Bps = I2C_BAUDRATE;
+
+    sourceClock = CLOCK_GetFreq(I2C0_CLK_SRC);
+
+    I2C_MasterInit(I2C0, &masterConfig, sourceClock);
+}
+
+static void i2c_release_bus_delay(void)
+{
+    uint32_t i = 0;
+    for (i = 0; i < I2C_RELEASE_BUS_COUNT; i++)
+    {
+        __NOP();
+    }
+}
+
+void i2c_releaseBus(void)
+{
+    uint8_t i = 0;
+    gpio_pin_config_t pin_config;
+    port_pin_config_t i2c_pin_config = {0};
+
+    /* Config pin mux as gpio */
+    i2c_pin_config.pullSelect = kPORT_PullUp;
+    i2c_pin_config.mux = kPORT_MuxAsGpio;
+
+    pin_config.pinDirection = kGPIO_DigitalOutput;
+    pin_config.outputLogic = 1U;
+    CLOCK_EnableClock(kCLOCK_PortE);
+    PORT_SetPinConfig(I2C_RELEASE_SCL_PORT, I2C_RELEASE_SCL_PIN, &i2c_pin_config);
+    PORT_SetPinConfig(I2C_RELEASE_SDA_PORT, I2C_RELEASE_SDA_PIN, &i2c_pin_config);
+
+    GPIO_PinInit(I2C_RELEASE_SCL_GPIO, I2C_RELEASE_SCL_PIN, &pin_config);
+    GPIO_PinInit(I2C_RELEASE_SDA_GPIO, I2C_RELEASE_SDA_PIN, &pin_config);
+
+    /* Drive SDA low first to simulate a start */
+    GPIO_PinWrite(I2C_RELEASE_SDA_GPIO, I2C_RELEASE_SDA_PIN, 0U);
+    i2c_release_bus_delay();
+
+    /* Send 9 pulses on SCL and keep SDA high */
+    for (i = 0; i < 9; i++)
+    {
+        GPIO_PinWrite(I2C_RELEASE_SCL_GPIO, I2C_RELEASE_SCL_PIN, 0U);
+        i2c_release_bus_delay();
+
+        GPIO_PinWrite(I2C_RELEASE_SDA_GPIO, I2C_RELEASE_SDA_PIN, 1U);
+        i2c_release_bus_delay();
+
+        GPIO_PinWrite(I2C_RELEASE_SCL_GPIO, I2C_RELEASE_SCL_PIN, 1U);
+        i2c_release_bus_delay();
+        i2c_release_bus_delay();
+    }
+
+    /* Send stop */
+    GPIO_PinWrite(I2C_RELEASE_SCL_GPIO, I2C_RELEASE_SCL_PIN, 0U);
+    i2c_release_bus_delay();
+
+    GPIO_PinWrite(I2C_RELEASE_SDA_GPIO, I2C_RELEASE_SDA_PIN, 0U);
+    i2c_release_bus_delay();
+
+    GPIO_PinWrite(I2C_RELEASE_SCL_GPIO, I2C_RELEASE_SCL_PIN, 1U);
+    i2c_release_bus_delay();
+
+    GPIO_PinWrite(I2C_RELEASE_SDA_GPIO, I2C_RELEASE_SDA_PIN, 1U);
+    i2c_release_bus_delay();
+}
+
 /*==================[external functions definition]==========================*/
 extern void bsp_frdmkl46z_i2c_init(void)
 {
+    i2c_releaseBus();
+    i2c_configPins();
+    i2c_config();
+
     efHal_dh_I2C0 = efHal_i2c_deviceReg(bsp_frdmkl46z_i2c_deviceTransfer, I2C0);
 
     I2C_MasterTransferCreateHandle(I2C0, &m_handle, i2c_master_callback, efHal_dh_I2C0);
