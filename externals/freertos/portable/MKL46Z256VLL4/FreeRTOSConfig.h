aa82722eee17a39e451f1ecea663fa98e526192e
Gustavo Muro <gustmuro@gmail.com>
2022-11-01 22:55:45 -0300

    set default tick hook to 0

--- externals/freertos/portable/MKL46Z256VLL4/FreeRTOSConfig.h
+++ externals/freertos/portable/MKL46Z256VLL4/FreeRTOSConfig.h
@@ -73,7 +73,7 @@
 
 /* Hook function related definitions. */
 #define configUSE_IDLE_HOOK                     0
-#define configUSE_TICK_HOOK                     1
+#define configUSE_TICK_HOOK                     0
 #define configCHECK_FOR_STACK_OVERFLOW          2
 #define configUSE_MALLOC_FAILED_HOOK            0
 #define configUSE_DAEMON_TASK_STARTUP_HOOK      0
