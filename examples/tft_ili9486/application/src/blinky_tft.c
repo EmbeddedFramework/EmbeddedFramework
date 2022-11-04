59d4a95523eaa232ca8d4f54508f1474c3051605
Gustavo Muro <gustmuro@gmail.com>
2022-11-04 13:01:03 -0300

    change example tft

--- examples/tft_ili9486/application/src/blinky_tft.c
+++ examples/tft_ili9486/application/src/blinky_tft.c
@@ -52,48 +52,17 @@
 /*==================[internal functions declaration]=========================*/
 
 /*==================[internal data definition]===============================*/
-
-/*==================[external data definition]===============================*/
-
-/*==================[internal functions definition]==========================*/
-
 static lv_disp_draw_buf_t draw_buf;
 static lv_color_t buf1[BUF_SIZE];
 static lv_disp_drv_t disp_drv;        /*Descriptor of a display driver*/
 static lv_indev_drv_t indev_drv;
 static int timeDownTouch;
 
-void lv_example_keyboard_2(void)
-{
-    /*Create an AZERTY keyboard map*/
-    static const char * kb_map[] = {"A", "Z", "E", "R", "T", "Y", "U", "I", "O", "P", LV_SYMBOL_BACKSPACE, "\n",
-                                    "Q", "S", "D", "F", "G", "J", "K", "L", "M",  LV_SYMBOL_NEW_LINE, "\n",
-                                    "W", "X", "C", "V", "B", "N", ",", ".", ":", "!", "?", "\n",
-                                    LV_SYMBOL_CLOSE, " ",  " ", " ", LV_SYMBOL_OK, NULL
-                                   };
+/*==================[external data definition]===============================*/
 
-    /*Set the relative width of the buttons and other controls*/
-    static const lv_btnmatrix_ctrl_t kb_ctrl[] = {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 6,
-                                                  4, 4, 4, 4, 4, 4, 4, 4, 4, 6,
-                                                  4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-                                                  2, LV_BTNMATRIX_CTRL_HIDDEN | 2, 6, LV_BTNMATRIX_CTRL_HIDDEN | 2, 2
-                                                 };
+/*==================[internal functions definition]==========================*/
 
-    /*Create a keyboard and add the new map as USER_1 mode*/
-    lv_obj_t * kb = lv_keyboard_create(lv_scr_act());
-
-    lv_keyboard_set_map(kb, LV_KEYBOARD_MODE_USER_1, kb_map, kb_ctrl);
-    lv_keyboard_set_mode(kb, LV_KEYBOARD_MODE_USER_1);
-
-    /*Create a text area. The keyboard will write here*/
-    lv_obj_t * ta;
-    ta = lv_textarea_create(lv_scr_act());
-    lv_obj_align(ta, LV_ALIGN_TOP_MID, 0, 10);
-    lv_obj_set_size(ta, lv_pct(90), 80);
-    lv_obj_add_state(ta, LV_STATE_FOCUSED);
-
-    lv_keyboard_set_textarea(kb, ta);
-}
+void lv_example_btnmatrix_2(void);
 
 static void blinky_task(void *pvParameters)
 {
@@ -102,6 +71,7 @@ static void blinky_task(void *pvParameters)
     touchScreen_init(TFT_XM, TFT_XP, TFT_YM, TFT_YP);
     touchScreen_conf(350, 3660, 550, 3558, MY_DISP_HOR_RES, MY_DISP_VER_RES);
     touchScreen_swapXY(1);
+    touchScreen_enablePullUP(1);
 
     lv_init();
 
@@ -119,7 +89,7 @@ static void blinky_task(void *pvParameters)
     indev_drv.read_cb = touchScreen_read;
     lv_indev_drv_register(&indev_drv);    /*Register the driver in LVGL */
 
-    lv_example_keyboard_2();
+    lv_example_btnmatrix_2();
 
     for (;;)
     {
