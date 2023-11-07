e4fb2739df8616fa97b20e917bcfada015dc1645
guidocicconi <guidoocicconi@gmail.com>
2023-11-07 12:43:08 -0300

    tabs replaced by 4 spaces

--- modules/bsp_frdmkl46z/src/bsp_frdmkl46z_pwm.c
+++ modules/bsp_frdmkl46z/src/bsp_frdmkl46z_pwm.c
@@ -48,9 +48,9 @@
 
 typedef struct
 {
-	TPM_Type *tpm;
-	tpm_chnl_t chnl;
-	efHal_gpio_id_t gpio;
+    TPM_Type *tpm;
+    tpm_chnl_t chnl;
+    efHal_gpio_id_t gpio;
 }pwmStruct_t;
 
 /*==================[internal functions declaration]=========================*/
@@ -61,10 +61,10 @@ static float _dutyPercentage[EF_HAL_PWM_TOTAL] = {0};
 
 static const pwmStruct_t pwmStruct[] =
 {
-    {TPM1, 0, EF_HAL_D3},     			  /* EF_HAL_PWM0 */
-	{TPM0, 2, EF_HAL_D5},     			  /* EF_HAL_PWM1 */
-	{TPM0, 4, EF_HAL_D6},     			  /* EF_HAL_PWM2 */
-	{TPM0, 1, EF_HAL_D9},     			  /* EF_HAL_PWM3 */
+    {TPM1, 0, EF_HAL_D3},                   /* EF_HAL_PWM0 */
+    {TPM0, 2, EF_HAL_D5},                   /* EF_HAL_PWM1 */
+    {TPM0, 4, EF_HAL_D6},                   /* EF_HAL_PWM2 */
+    {TPM0, 1, EF_HAL_D9},                   /* EF_HAL_PWM3 */
     {TPM0, 5, EF_HAL_GPIO_LED_GREEN},     /* EF_HAL_GPIO_LED_GREEN */
     {TPM0, 2, EF_HAL_GPIO_LED_RED}        /* EF_HAL_GPIO_LED_RED */
 };
@@ -76,58 +76,58 @@ static tpm_chnl_pwm_signal_param_t tpm_chnl_pwm_signal_param[6];
 
 /*==================[internal functions definition]==========================*/
 bool setDuty (efHal_pwm_id_t id, uint32_t dutyCount){
-	_dutyPercentage[id] = ((float)dutyCount/(float)pwmStruct[id].tpm->MOD*(float)100 + 1);
-	uint16_t  mod = pwmStruct[id].tpm->MOD;
+    _dutyPercentage[id] = ((float)dutyCount/(float)pwmStruct[id].tpm->MOD*(float)100 + 1);
+    uint16_t  mod = pwmStruct[id].tpm->MOD;
 
-	xSemaphoreTake(mutex, portMAX_DELAY);
+    xSemaphoreTake(mutex, portMAX_DELAY);
 
-	bsp_frdmkl46z_internal_gpio_confAsPWM(pwmStruct[id].gpio);
+    bsp_frdmkl46z_internal_gpio_confAsPWM(pwmStruct[id].gpio);
 
-	if(dutyCount >= 0){
-		if (dutyCount >= mod) pwmStruct[id].tpm->CONTROLS[pwmStruct[id].chnl].CnV = mod + 1;
-		else if(id == EF_HAL_PWM_LED_RED || id == EF_HAL_PWM_LED_GREEN)
-			pwmStruct[id].tpm->CONTROLS[pwmStruct[id].chnl].CnV = mod - dutyCount;
-		else
-			pwmStruct[id].tpm->CONTROLS[pwmStruct[id].chnl].CnV = dutyCount;
-	}
+    if(dutyCount >= 0){
+        if (dutyCount >= mod) pwmStruct[id].tpm->CONTROLS[pwmStruct[id].chnl].CnV = mod + 1;
+        else if(id == EF_HAL_PWM_LED_RED || id == EF_HAL_PWM_LED_GREEN)
+            pwmStruct[id].tpm->CONTROLS[pwmStruct[id].chnl].CnV = mod - dutyCount;
+        else
+            pwmStruct[id].tpm->CONTROLS[pwmStruct[id].chnl].CnV = dutyCount;
+    }
 
-	xSemaphoreGive(mutex);
+    xSemaphoreGive(mutex);
 
-	return 1;
+    return 1;
 }
 
 bool setPeriod (efHal_pwm_id_t id, uint32_t period_nS){
-	uint16_t dutyCount = 0;
+    uint16_t dutyCount = 0;
 
-	xSemaphoreTake(mutex, portMAX_DELAY);
+    xSemaphoreTake(mutex, portMAX_DELAY);
 
-	TPM_StopTimer(pwmStruct[id].tpm);
+    TPM_StopTimer(pwmStruct[id].tpm);
 
-	pwmStruct[id].tpm->MOD = (uint32_t)(((float)CLOCK_GetFreq(kCLOCK_PllFllSelClk)/(float)(1U << (pwmStruct[id].tpm->SC & TPM_SC_PS_MASK)) * (float)period_nS * (float)1e-9) - 1);
+    pwmStruct[id].tpm->MOD = (uint32_t)(((float)CLOCK_GetFreq(kCLOCK_PllFllSelClk)/(float)(1U << (pwmStruct[id].tpm->SC & TPM_SC_PS_MASK)) * (float)period_nS * (float)1e-9) - 1);
 
-	dutyCount = _dutyPercentage[id] * pwmStruct[id].tpm->MOD;
+    dutyCount = _dutyPercentage[id] * pwmStruct[id].tpm->MOD;
 
-	if(dutyCount >= 0){
-		if (dutyCount >= pwmStruct[id].tpm->MOD) pwmStruct[id].tpm->CONTROLS[pwmStruct[id].chnl].CnV = pwmStruct[id].tpm->MOD + 1;
-		else if(id == EF_HAL_PWM_LED_RED || id == EF_HAL_PWM_LED_GREEN)
-			pwmStruct[id].tpm->CONTROLS[pwmStruct[id].chnl].CnV = pwmStruct[id].tpm->MOD - dutyCount;
-		else
-			pwmStruct[id].tpm->CONTROLS[pwmStruct[id].chnl].CnV = dutyCount;
-	}
+    if(dutyCount >= 0){
+        if (dutyCount >= pwmStruct[id].tpm->MOD) pwmStruct[id].tpm->CONTROLS[pwmStruct[id].chnl].CnV = pwmStruct[id].tpm->MOD + 1;
+        else if(id == EF_HAL_PWM_LED_RED || id == EF_HAL_PWM_LED_GREEN)
+            pwmStruct[id].tpm->CONTROLS[pwmStruct[id].chnl].CnV = pwmStruct[id].tpm->MOD - dutyCount;
+        else
+            pwmStruct[id].tpm->CONTROLS[pwmStruct[id].chnl].CnV = dutyCount;
+    }
 
-	TPM_StartTimer(pwmStruct[id].tpm, kTPM_SystemClock);
+    TPM_StartTimer(pwmStruct[id].tpm, kTPM_SystemClock);
 
-	xSemaphoreGive(mutex);
+    xSemaphoreGive(mutex);
 
-	return 1;
+    return 1;
 }
 
 uint32_t getPeriodCount (efHal_pwm_id_t id){
-	return pwmStruct[id].tpm->MOD;
+    return pwmStruct[id].tpm->MOD;
 }
 
 uint32_t getPeriodNs (efHal_pwm_id_t id){
-	return (uint32_t)(((float)pwmStruct[id].tpm->MOD+1)*(float)1e9/((float)CLOCK_GetFreq(kCLOCK_PllFllSelClk)/(float)(1U << (pwmStruct[id].tpm->SC & TPM_SC_PS_MASK))));
+    return (uint32_t)(((float)pwmStruct[id].tpm->MOD+1)*(float)1e9/((float)CLOCK_GetFreq(kCLOCK_PllFllSelClk)/(float)(1U << (pwmStruct[id].tpm->SC & TPM_SC_PS_MASK))));
 }
 
 void confIntCount (efHal_pwm_id_t id, uint32_t count){
@@ -138,7 +138,7 @@ void confIntCount (efHal_pwm_id_t id, uint32_t count){
 /*==================[external functions definition]==========================*/
 extern void bsp_frdmkl46z_pwm_init(void)
 {
-	efHal_pwm_callBacks_t cb;
+    efHal_pwm_callBacks_t cb;
     tpm_config_t tpm0_config, tpm1_config;
 
     mutex = xSemaphoreCreateMutex();
@@ -147,17 +147,17 @@ extern void bsp_frdmkl46z_pwm_init(void)
     CLOCK_SetTpmClock(1U);
 
     /*
-	* tpm_config.prescale = kTPM_Prescale_Divide_1;
-	* tpm_config.useGlobalTimeBase = false;
-	* tpm_config.enableDoze = false;
-	* tpm_config.enableDebugMode = false;
-	* tpm_config.enableReloadOnTrigger = false;
-	* tpm_config.enableStopOnOverflow = false;
-	* tpm_config.enableStartOnTrigger = false;
-	* tpm_config.enablePauseOnTrigger = false;
-	* tpm_config.triggerSelect = kTPM_Trigger_Select_0;
-	* tpm_config.triggerSource = kTPM_TriggerSource_External;
-	*/
+    * tpm_config.prescale = kTPM_Prescale_Divide_1;
+    * tpm_config.useGlobalTimeBase = false;
+    * tpm_config.enableDoze = false;
+    * tpm_config.enableDebugMode = false;
+    * tpm_config.enableReloadOnTrigger = false;
+    * tpm_config.enableStopOnOverflow = false;
+    * tpm_config.enableStartOnTrigger = false;
+    * tpm_config.enablePauseOnTrigger = false;
+    * tpm_config.triggerSelect = kTPM_Trigger_Select_0;
+    * tpm_config.triggerSource = kTPM_TriggerSource_External;
+    */
     TPM_GetDefaultConfig(&tpm0_config);
     TPM_GetDefaultConfig(&tpm1_config);
 
