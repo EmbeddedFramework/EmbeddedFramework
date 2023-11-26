307768a99600015680c85e5ee525be867bdd6799
Gustavo Muro <gustmuro@gmail.com>
2023-11-26 16:19:34 -0300

    #23 enable adc interrupt

--- modules/bsp_nucleoF767ZI/internals/Core/Inc/stm32f7xx_it.h
+++ modules/bsp_nucleoF767ZI/internals/Core/Inc/stm32f7xx_it.h
@@ -52,6 +52,7 @@ void MemManage_Handler(void);
 void BusFault_Handler(void);
 void UsageFault_Handler(void);
 void DebugMon_Handler(void);
+void ADC_IRQHandler(void);
 void TIM1_UP_TIM10_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
--- modules/bsp_nucleoF767ZI/internals/Core/Src/stm32f7xx_it.c
+++ modules/bsp_nucleoF767ZI/internals/Core/Src/stm32f7xx_it.c
@@ -55,6 +55,7 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern ADC_HandleTypeDef hadc1;
 extern TIM_HandleTypeDef htim1;
 
 /* USER CODE BEGIN EV */
@@ -158,6 +159,20 @@ void DebugMon_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32f7xx.s).                    */
 /******************************************************************************/
+
+/**
+  * @brief This function handles ADC1, ADC2 and ADC3 global interrupts.
+  */
+void ADC_IRQHandler(void)
+{
+  /* USER CODE BEGIN ADC_IRQn 0 */
+
+  /* USER CODE END ADC_IRQn 0 */
+  HAL_ADC_IRQHandler(&hadc1);
+  /* USER CODE BEGIN ADC_IRQn 1 */
+
+  /* USER CODE END ADC_IRQn 1 */
+}
 
 /**
   * @brief This function handles TIM1 update interrupt and TIM10 global interrupt.
