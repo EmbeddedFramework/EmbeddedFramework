fa924d10266030176f00b8dcdc1bc6bb9bbdbe59
Gustavo Muro <gustmuro@gmail.com>
2023-12-17 10:57:17 -0300

    #25 implement new analog api

--- modules/efHal/src/efHal_analog.c
+++ modules/efHal/src/efHal_analog.c
@@ -49,6 +49,8 @@ typedef struct
 
 /*==================[internal data definition]===============================*/
 static taskHandle_analog_t taskHandle_analog[EF_HAL_ANALOG_TOTAL_WAIT_CONV];
+static efHal_analog_endConvCB_t endConvCB[EF_HAL_ANALOG_TOTAL_CALL_BACK_CONV];
+
 static efHal_analog_callBacks_t callBacks;
 
 /*==================[external data definition]===============================*/
@@ -66,6 +68,10 @@ extern void efHal_analog_init(void)
         taskHandle_analog[i].taskHandle = NULL;
         taskHandle_analog[i].gpioId = EF_HAL_INVALID_ID;
     }
+    for (i = 0 ; i < EF_HAL_ANALOG_TOTAL_CALL_BACK_CONV ; i++)
+    {
+        endConvCB[i]= NULL;
+    }
 }
 
 extern void efHal_analog_confAsAnalog(efHal_gpio_id_t id)
@@ -78,20 +84,21 @@ extern void efHal_analog_confAsAnalog(efHal_gpio_id_t id)
     }
 }
 
-extern bool efHal_analog_startConv(efHal_gpio_id_t id)
+extern bool efHal_analog_startWaitRead(efHal_gpio_id_t id, int32_t *result, TickType_t xBlockTime)
 {
-    bool ret = false;
-    int i;
+    bool ret = true;
+    int slot;
 
+    /* search for free slot */
     taskENTER_CRITICAL();
 
-    for (i = 0 ; i < EF_HAL_ANALOG_TOTAL_WAIT_CONV ; i++)
+    for (slot = 0 ; slot < EF_HAL_ANALOG_TOTAL_WAIT_CONV ; slot++)
     {
-        if (taskHandle_analog[i].taskHandle == NULL)
+        if (taskHandle_analog[slot].gpioId == EF_HAL_INVALID_ID)
         {
-            taskHandle_analog[i].gpioId = id;
-            taskHandle_analog[i].taskHandle = xTaskGetCurrentTaskHandle();
-            xTaskNotifyStateClear(taskHandle_analog[i].taskHandle);
+            taskHandle_analog[slot].gpioId = id;
+            taskHandle_analog[slot].taskHandle = xTaskGetCurrentTaskHandle();
+            xTaskNotifyStateClear(taskHandle_analog[slot].taskHandle);
             break;
         }
     }
@@ -99,49 +106,83 @@ extern bool efHal_analog_startConv(efHal_gpio_id_t id)
     taskEXIT_CRITICAL();
 
     /* check if no free slot error */
-    if (i >= EF_HAL_ANALOG_TOTAL_WAIT_CONV)
-        efErrorHdl_error(EF_ERROR_HDL_NO_FREE_SLOT, "callBacks.startConv");
-    else if (callBacks.startConv != NULL)
-        ret = callBacks.startConv(id);
-    else
-        efErrorHdl_error(EF_ERROR_HDL_NULL_POINTER, "callBacks.startConv");
-
-    return ret;
-}
-
-extern bool efHal_analog_waitConv(efHal_gpio_id_t id, TickType_t xBlockTime)
-{
-    bool ret = false;
-
-    if (ulTaskNotifyTake(pdTRUE, xBlockTime))
-        ret = true;
-
-    return ret;
-}
-
-extern int32_t efHal_analog_read(efHal_gpio_id_t id)
-{
-    int i;
-    int32_t ret = 0;
-
-    if (callBacks.aRead != NULL)
-        ret = callBacks.aRead(id);
-    else
+    if (slot >= EF_HAL_ANALOG_TOTAL_WAIT_CONV)
     {
-        efErrorHdl_error(EF_ERROR_HDL_NULL_POINTER, "callBacks.aRead");
+        ret = false;
+        efErrorHdl_error(EF_ERROR_HDL_NO_FREE_SLOT, "callBacks.startConv");
+    }
+
+    /* start conversion */
+    if (ret)
+        ret = efHal_analog_startConv(id);
+
+    /* wait for end conversion */
+    if (ret)
+        ret = ulTaskNotifyTake(pdTRUE, xBlockTime);
+
+    if (ret)
+    {
+        /* read value */
+        if (callBacks.aRead != NULL)
+            *result = callBacks.aRead(id);
+        else
+        {
+            ret = false;
+            efErrorHdl_error(EF_ERROR_HDL_NULL_POINTER, "callBacks.aRead");
+        }
     }
 
     /* release slot */
     taskENTER_CRITICAL();
-    for (i = 0 ; i < EF_HAL_ANALOG_TOTAL_WAIT_CONV ; i++)
+    if (slot < EF_HAL_ANALOG_TOTAL_WAIT_CONV)
     {
-        if (taskHandle_analog[i].gpioId == id)
-        {
-            taskHandle_analog[i].gpioId = EF_HAL_INVALID_ID;
-            taskHandle_analog[i].taskHandle = NULL;
-        }
+        taskHandle_analog[slot].gpioId = EF_HAL_INVALID_ID;
+        taskHandle_analog[slot].taskHandle = NULL;
     }
     taskEXIT_CRITICAL();
+
+    return ret;
+}
+
+extern bool efHal_analog_setEndConvCB(efHal_analog_endConvCB_t ecCB)
+{
+    bool ret = true;
+    int slot;
+
+    /* search for free slot */
+    taskENTER_CRITICAL();
+
+    for (slot = 0 ; slot < EF_HAL_ANALOG_TOTAL_CALL_BACK_CONV ; slot++)
+    {
+        if (endConvCB[slot] == NULL)
+        {
+            endConvCB[slot] = ecCB;
+            break;
+        }
+    }
+
+    taskEXIT_CRITICAL();
+
+    /* check if no free slot error */
+    if (slot >= EF_HAL_ANALOG_TOTAL_CALL_BACK_CONV)
+    {
+        ret = false;
+        efErrorHdl_error(EF_ERROR_HDL_NO_FREE_SLOT, "callBacks.setEndConvCB");
+    }
+
+    return ret;
+}
+
+extern bool efHal_analog_startConv(efHal_gpio_id_t id)
+{
+    bool ret = false;
+
+    if (callBacks.startConv != NULL)
+        ret = callBacks.startConv(id);
+    else
+    {
+        efErrorHdl_error(EF_ERROR_HDL_NULL_POINTER, "callBacks.startConv");
+    }
 
     return ret;
 }
@@ -178,11 +219,15 @@ extern void efHal_internal_analog_endConvInterruptRoutine(efHal_gpio_id_t id)
             {
                 vTaskNotifyGiveFromISR(taskHandle_analog[i].taskHandle, &xHigherPriorityTaskWoken);
             }
-            else
-            {
-                efErrorHdl_error(EF_ERROR_HDL_NULL_POINTER, "taskHandle");
-            }
             break;
+        }
+    }
+
+    for (i = 0 ; i < EF_HAL_ANALOG_TOTAL_CALL_BACK_CONV ; i++)
+    {
+        if (endConvCB[i] != NULL)
+        {
+            endConvCB[i](id, callBacks.aRead(id));
         }
     }
 
