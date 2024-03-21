d594d31b661b897a5447814ee00f31c3717a5f87
Gustavo Muro <gustmuro@gmail.com>
2024-03-21 12:36:56 -0300

    #31 work in test report

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -3,13 +3,26 @@
 # Lista de directorios de módulos que contienen scripts de pruebas
 MODULE_DIRS=("modules/template")
 
+# Crear directorio para los informes de prueba
+mkdir -p out/testReport
+
 # Itera sobre cada directorio de módulo y ejecuta su script de pruebas
 for module_dir in "${MODULE_DIRS[@]}"; do
     if [ -f "${module_dir}/test/run_tests.sh" ]; then
         echo "Ejecutando pruebas para el módulo: ${module_dir}..."
         (./$module_dir/test/run_tests.sh)
+        
+        # Copiar carpeta build/html a out/testReport/
+        if [ -d "${module_dir}/test/build/html" ]; then
+            cp -r "${module_dir}/test/build/html" out/testReport/$(basename $module_dir)
+        else
+            echo "No se encontró la carpeta build/html para el módulo: ${module_dir}"
+        fi
+        
     else
         echo "No se encontró el script de pruebas para el módulo: ${module_dir}"
     fi
 done
+
+./genTestReport.sh
 
