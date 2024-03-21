91afc56d8ac76081659b44e6b39b62e0814a6d57
Gustavo Muro <gustmuro@gmail.com>
2024-03-21 10:40:04 -0300

    #31 work in test script

---
+++ modules/template/test/run_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# prepara nombres de directorios
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+HTML_DIR="build/html"
+SUPPORT_DIR="build/support"
+
+# se para sobre el directorio test
+cd "$SCRIPT_DIR" || exit
+
+if [ ! -d "$HTML_DIR" ]; then
+    mkdir -p "$HTML_DIR"
+fi
+
+if [ ! -d "$SUPPORT_DIR" ]; then
+    mkdir -p "$SUPPORT_DIR"
+fi
+
+ceedling test:all
+ceedling gcov:all
+gcovr -r . --html --html-details -o $HTML_DIR/coverage_report.html
+
